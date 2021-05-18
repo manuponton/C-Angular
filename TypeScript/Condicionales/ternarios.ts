442382c0da397344e9581d96dba7d3731e5329de
Manuel Ponton <manuel.ponton@zabud.com.co>
2021-05-18 12:58:50 -0500

    Uso de ternarios.ts

---
+++ TypeScript/Condicionales/ternarios.ts
@@ -0,0 +1,7 @@
+var nombre: string, edad: number = 15
+if(edad>10){
+    nombre= "Juan"
+    console.log(nombre)
+}
+nombre = edad > 10 ? "Juan de ternario": "No se cumplió condicional"
+console.log(nombre)
