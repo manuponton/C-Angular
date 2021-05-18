552c3925bf068ee7958402905db5db62a8cfc164
Manuel Ponton <manuel.ponton@zabud.com.co>
2021-05-18 12:27:04 -0500

    Se mueve a carpeta para organizar proyecto

---
+++ TypeScript/TiposDeDatos/any.js
@@ -0,0 +1,7 @@
+var nombre = "15 canciones";
+var canciones;
+canciones = "Musica";
+canciones = 25;
+var cancionesRecibidas;
+//Se le puede asignar cualquier tipo de dato
+//Su uso es frecuente cuando se reciben datos de apis externas
---
+++ TypeScript/TiposDeDatos/interfaces.js
@@ -0,0 +1,6 @@
+var persona1 = {
+    nombre: "Manuel",
+    apellido: "Caicedo",
+    peso: 15,
+    edad: 50
+};
