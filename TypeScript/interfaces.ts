25c605f2527836fac3b3a534420996381e0e746a
Manuel Ponton <manuel.ponton@zabud.com.co>
2021-05-18 12:19:37 -0500

    Uso de interfaces.ts

---
+++ TypeScript/interfaces.ts
@@ -0,0 +1,12 @@
+interface Personas {
+    nombre: string,
+    apellido: string,
+    edad: number,
+    peso: number
+}
+var persona1: Personas = {
+    nombre: "Manuel",
+    apellido: "Caicedo",
+    peso: 15,
+    edad: 50
+}
