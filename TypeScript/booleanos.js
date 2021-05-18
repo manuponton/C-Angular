98891622c0454236c06a744b3c39ae59b886f520
Manuel Ponton <manuel.ponton@zabud.com.co>
2021-05-18 12:09:05 -0500

    Uso de booleanos.ts

---
+++ TypeScript/booleanos.js
@@ -0,0 +1,9 @@
+var esNuevo = false;
+var deseaImprimir = true;
+console.log(esNuevo);
+console.log(deseaImprimir);
+var aceptaDecuento = (15 + 55 == 33);
+console.log(aceptaDecuento);
+var juan = "Juan", juan2 = "juan";
+var sonNombresIguales = juan == juan2;
+console.log("las variables juan son: " + sonNombresIguales);
---
+++ TypeScript/booleanos.ts
@@ -0,0 +1,12 @@
+var esNuevo: boolean = false
+var deseaImprimir: boolean = true
+console.log(esNuevo)
+console.log(deseaImprimir)
+
+var aceptaDecuento: boolean =(15 + 55 == 33)
+console.log(aceptaDecuento)
+
+var juan : string = "Juan", juan2: string = "juan"
+var sonNombresIguales: boolean =  juan==juan2
+console.log("las variables juan son: "+ sonNombresIguales)
+
