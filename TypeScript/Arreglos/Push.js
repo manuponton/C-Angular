4ffa523e6e487d284078fd9ac9938505b94a4ae8
Manuel Ponton <manuel.ponton@zabud.com.co>
2021-06-18 15:33:04 -0500

    Uso de push en Arreglo

---
+++ TypeScript/Arreglos/Push.js
@@ -0,0 +1,3 @@
+var nombres = ['ana', 'maria', 'juan'];
+nombres.push('Manuel');
+console.log(nombres);
---
+++ TypeScript/Arreglos/Push.ts
@@ -0,0 +1,9 @@
+var nombres = ['ana','maria','juan']
+nombres.push('Manuel')
+console.log(nombres)
+interface Alumno {
+    nombre: String,
+    apellido: String
+}
+var alumnos : Alumno []= []
+alumnos.push({nombre:'Manuel',apellido:'Caicedo'})
